da168b72580975121122e6652145d69dce7171d0
personal <primastech101@gmail.com>
2023-12-28 13:23:21 +0100

    completed refactoring by using WPCS

---
+++ includes/class-rank-math-dashboard-widget.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Rank Math Dashboard Widget
+ *
+ * @package rankmath-test
+ */
+
+/**
+ * Rank_Math_Dashboard_Widget Class
+ *
+ * This class handles the creation and functionality of the Rank Math Dashboard Widget.
+ *
+ * @package rankmath-test
+ */
+class Rank_Math_Dashboard_Widget {
+
+	/** This is the table name for the dashboard data.
+	 *
+	 * @var string $table_name
+	 */
+	public $table_name;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		global $wpdb;
+
+		$this->table_name = $wpdb->prefix . 'rm_dashboard_data';
+
+		add_action( 'wp_dashboard_setup', array( $this, 'rankmathtest_dashboard_init' ) );
+
+		if ( is_admin() && admin_url( '/' ) ) {
+			add_action( 'admin_enqueue_scripts', array( $this, 'rankmathtest_enqueue_scripts' ) );
+		}
+		add_action( 'rest_api_init', array( $this, 'register_custom_rest_api_route' ) );
+	}
+
+	/** Instantiates the Class.
+	 *
+	 * @return self|null
+	 */
+	public static function get_instance(): self|null {
+		static $instance = null;
+
+		if ( is_null( $instance ) ) {
+			$instance = new self();
+		}
+
+		return $instance;
+	}
+
+	/** This functions adds the dashboard widget.
+	 *
+	 * @return void
+	 */
+	public function rankmathtest_dashboard_init(): void {
+		wp_add_dashboard_widget( 'dashboard_widget', 'Rank Math Widget', array( $this, 'rankmath_widget_init' ) );
+	}
+
+	/** This function addes the template/main.php.
+	 *
+	 * @return void
+	 */
+	public function rankmath_widget_init(): void {
+		require_once RANKMATHTEST__PLUGIN_DIR . 'templates/main.php';
+	}
+
+	/** This functions enqueue scripts.
+	 *
+	 * @return void
+	 */
+	public function rankmathtest_enqueue_scripts(): void {
+
+		$style_file_path = RANKMATHTEST__PLUGIN_DIR . '/build/index.css';
+		$style_version   = filemtime( $style_file_path );
+
+		wp_enqueue_script( 'wp-api' );
+		wp_enqueue_style( 'rankmathtest_style', RANKMATHTEST__PLUGIN_URL . '/build/index.css', array(), $style_version );
+		wp_enqueue_script( 'rankmathtest_script', RANKMATHTEST__PLUGIN_URL . '/build/index.js', array( 'wp-element', 'wp-api-fetch', 'wp-i18n', 'wp-components' ), '1.0.0', true );
+	}
+
+	/** This function creates the rest api endpoint for the plugin.
+	 *
+	 * @return void
+	 */
+	public function register_custom_rest_api_route(): void {
+
+		register_rest_route(
+			'react/v1',
+			'/data/(?P<duration>[a-zA-Z0-9-]+)',
+			array(
+				'methods'             => 'GET',
+				'callback'            => array( $this, 'rankmathtest_get_data' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+	}
+
+
+	/**
+	 * Fetches data from the database based on the specified period.
+	 *
+	 * @param string $period The period for which data needs to be fetched.
+	 * @return array An array of formatted output containing name, uv, pv, and amt.
+	 */
+	public function fetch_db( $period ): array {
+		global $wpdb;
+
+		$formatted_output = array();
+
+		$table_name = $this->table_name;
+
+		$sql_query = $wpdb->prepare(
+        // phpcs:ignore WordPress.DB.PreparedSQLPlaceholders.UnquotedComplexPlaceholder
+			'SELECT * FROM %s WHERE period = %s',
+			$table_name,
+			$period
+		);
+
+		$results = $wpdb->get_results( $sql_query, ARRAY_A );
+
+		if ( $results ) {
+			foreach ( $results as $row ) {
+				$formatted_output[] = array(
+					'name' => $row['name'],
+					'uv'   => $row['uv'],
+					'pv'   => $row['pv'],
+					'amt'  => $row['amt'],
+				);
+			}
+		}
+
+		return $formatted_output;
+	}
+
+	/** Returns database data in json format.
+	 *
+	 * @param string $request This is the Url Parameter.
+	 * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+	 */
+	public function rankmathtest_get_data( $request ): WP_REST_Response {
+		$duration = esc_attr( $request['duration'] );
+
+		$response = $this->fetch_db( $duration );
+
+		return rest_ensure_response( $response );
+	}
+}
+
+// Instantiate Plugin.
+
+Rank_Math_Dashboard_Widget::get_instance();
--- rankmath-test.php
+++ rankmath-test.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: Rank Math Dashboard widget
  * Description: simple dashboard widget
@@ -8,52 +7,59 @@
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Abiodun Paul ogunnaike
+ *
+ * @package rankmath-test
  */
 
-if (!defined('ABSPATH')) {
-    wp_die(__('Do not open this file directly.', 'rankmath-test'));
+if ( ! defined( 'ABSPATH' ) ) {
+	wp_die( esc_html_e( 'Do not open this file directly.', 'rankmath-test' ) );
 }
 
+// Basic setup.
 
-// require 'vendor/autoload.php';
-//Basic setup
+define( 'RANKMATHTEST__PLUGIN_FILE', __FILE__ );
+define( 'RANKMATHTEST__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'RANKMATHTEST__PLUGIN_URL', plugins_url( '', RANKMATHTEST__PLUGIN_FILE ) );
 
-define('RANKMATHTEST__PLUGIN_FILE', __FILE__);
-define('RANKMATHTEST__PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('RANKMATHTEST__PLUGIN_URL', plugins_url("", RANKMATHTEST__PLUGIN_FILE));
-
-
-class Rank_Math_Test
-{
-
-    public $table_name;
-
-    public function __construct()
-    {
-        global $wpdb;
+/**
+ * Class Rank_Math_Test
+ *
+ * @package Rank_Math_Test
+ */
+class Rank_Math_Test {
 
 
-        //Include main plugin file
-        require_once RANKMATHTEST__PLUGIN_DIR . '/includes/class-rankmathdashboardwidget.php';
+	/** This is the table name for the dashboard data.
+	 *
+	 * @var string $table_name
+	 */
+	public $table_name;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		global $wpdb;
+
+		// Include main plugin file.
+		require_once RANKMATHTEST__PLUGIN_DIR . '/includes/class-rank-math-dashboard-widget.php';
+
+		$this->table_name = Rank_Math_Dashboard_Widget::get_instance()->table_name;
+		register_activation_hook( __FILE__, array( $this, 'install_create_table' ) );
+		register_activation_hook( __FILE__, array( $this, 'insert_dummy_data' ) );
+	}
 
 
+	/** This function creates a table on plugin activation.
+	 *
+	 * @return void
+	 */
+	public function install_create_table(): void {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name      = $this->table_name;
 
-
-        $this->table_name = Rank_Math_Dashboard_Widget::get_instance()->table_name;
-        register_activation_hook(__FILE__, [$this, 'install_create_table']);
-        register_activation_hook(__FILE__, [$this, 'insert_dummy_data']);
-    }
-
-
-
-    public function install_create_table()
-    {
-        global $wpdb;
-        $charset_collate = $wpdb->get_charset_collate();
-        $table_name = $this->table_name;
-
-
-        $sql = "CREATE TABLE  $table_name (
+		$sql = "CREATE TABLE  $table_name (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `period` varchar(25) NOT NULL,
       `name` varchar(25) NOT NULL,
@@ -63,200 +69,200 @@ class Rank_Math_Test
       PRIMARY KEY (`id`)
     ) $charset_collate;";
 
-
-        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        dbDelta($sql);
-    }
-
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( $sql );
+	}
 
 
-    function insert_dummy_data()
-    {
-        global $wpdb;
+	/** Inserts dummy data into the created table on activation.
+	 *
+	 * @return void
+	 */
+	public function insert_dummy_data(): void {
+		global $wpdb;
 
+		// 7days dummy data
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '7days',
+				'name'   => 'Page A',
+				'uv'     => 400,
+				'pv'     => 2400,
+				'amt'    => 2400,
+			)
+		);
 
-        //7days dummy data
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '7days',
-                'name' => 'Page A',
-                'uv' => 400,
-                'pv' => 2400,
-                'amt' => 2400
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '7days',
+				'name'   => 'Page B',
+				'uv'     => 300,
+				'pv'     => 200,
+				'amt'    => 200,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '7days',
-                'name' => 'Page B',
-                'uv' => 300,
-                'pv' => 200,
-                'amt' => 200
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '7days',
+				'name'   => 'Page C',
+				'uv'     => 500,
+				'pv'     => 4000,
+				'amt'    => 4000,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '7days',
-                'name' => 'Page C',
-                'uv' => 500,
-                'pv' => 4000,
-                'amt' => 4000
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '7days',
+				'name'   => 'Page D',
+				'uv'     => 330,
+				'pv'     => 1500,
+				'amt'    => 1500,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '7days',
-                'name' => 'Page D',
-                'uv' => 330,
-                'pv' => 1500,
-                'amt' => 1500
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '7days',
+				'name'   => 'Page D',
+				'uv'     => 100,
+				'pv'     => 1500,
+				'amt'    => 1500,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '7days',
-                'name' => 'Page D',
-                'uv' => 100,
-                'pv' => 1500,
-                'amt' => 1500
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '7days',
+				'name'   => 'Page E',
+				'uv'     => 100,
+				'pv'     => 2400,
+				'amt'    => 2400,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '7days',
-                'name' => 'Page E',
-                'uv' => 100,
-                'pv' => 2400,
-                'amt' => 2400
-            )
-        );
+		// 15days dummy data
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '15days',
+				'name'   => 'Page A',
+				'uv'     => 200,
+				'pv'     => 2000,
+				'amt'    => 3400,
+			)
+		);
 
-        //15days dummy data
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '15days',
-                'name' => 'Page A',
-                'uv' => 200,
-                'pv' => 2000,
-                'amt' => 3400
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '15days',
+				'name'   => 'Page B',
+				'uv'     => 600,
+				'pv'     => 3000,
+				'amt'    => 3000,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '15days',
-                'name' => 'Page B',
-                'uv' => 600,
-                'pv' => 3000,
-                'amt' => 3000
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '15days',
+				'name'   => 'Page C',
+				'uv'     => 400,
+				'pv'     => 4500,
+				'amt'    => 2000,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '15days',
-                'name' => 'Page C',
-                'uv' => 400,
-                'pv' => 4500,
-                'amt' => 2000
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '15days',
+				'name'   => 'Page D',
+				'uv'     => 630,
+				'pv'     => 1000,
+				'amt'    => 2500,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '15days',
-                'name' => 'Page D',
-                'uv' => 630,
-                'pv' => 1000,
-                'amt' => 2500
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '15days',
+				'name'   => 'Page E',
+				'uv'     => 200,
+				'pv'     => 1400,
+				'amt'    => 5400,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '15days',
-                'name' => 'Page E',
-                'uv' => 200,
-                'pv' => 1400,
-                'amt' => 5400
-            )
-        );
+		// 1month dummy data
 
-        //1month dummy data
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '1month',
+				'name'   => 'Page A',
+				'uv'     => 500,
+				'pv'     => 5000,
+				'amt'    => 1400,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '1month',
-                'name' => 'Page A',
-                'uv' => 500,
-                'pv' => 5000,
-                'amt' => 1400
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '1month',
+				'name'   => 'Page B',
+				'uv'     => 400,
+				'pv'     => 4000,
+				'amt'    => 3000,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '1month',
-                'name' => 'Page B',
-                'uv' => 400,
-                'pv' => 4000,
-                'amt' => 3000
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '1month',
+				'name'   => 'Page C',
+				'uv'     => 300,
+				'pv'     => 3500,
+				'amt'    => 2000,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '1month',
-                'name' => 'Page C',
-                'uv' => 300,
-                'pv' => 3500,
-                'amt' => 2000
-            )
-        );
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '1month',
+				'name'   => 'Page D',
+				'uv'     => 230,
+				'pv'     => 2000,
+				'amt'    => 5000,
+			)
+		);
 
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '1month',
-                'name' => 'Page D',
-                'uv' => 230,
-                'pv' => 2000,
-                'amt' => 5000
-            )
-        );
-
-        $wpdb->insert(
-            $this->table_name,
-            array(
-                'period'  => '1month',
-                'name' => 'Page E',
-                'uv' => 100,
-                'pv' => 1400,
-                'amt' => 1400
-            )
-        );
-    }
+		$wpdb->insert(
+			$this->table_name,
+			array(
+				'period' => '1month',
+				'name'   => 'Page E',
+				'uv'     => 100,
+				'pv'     => 1400,
+				'amt'    => 1400,
+			)
+		);
+	}
 }
 
 
-//Instantiate Plugin
+// Instantiate Plugin.
 $rank_math_test = new Rank_Math_Test();
